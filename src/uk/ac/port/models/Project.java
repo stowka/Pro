08619294a5cc2557029421b50ca3efbcbe88e057
Antoine De Gieter <antoine.degieter@me.com>
2014-04-02 16:20:10 +0100

    project

--- src/uk/ac/port/models/Project.java
+++ src/uk/ac/port/models/Project.java
@@ -1,5 +1,75 @@
 package uk.ac.port.models;
 
-public class Project {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Project {
+	private int id;
+	private String description;
+	private String creationTime, modificationTime;
+	private List<Molecule> molecules;
+	
+	public Project() {
+		
+	}
+	
+	public Project(int id) {
+		this.id = id;
+	}
+	
+	public Project(String description, String creationTime, String modificationTime, ArrayList<Molecule> molecules) {
+		this.description = description;
+		this.creationTime = creationTime;
+		this.modificationTime = modificationTime;
+		this.molecules = molecules;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(String creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public String getModificationTime() {
+		return modificationTime;
+	}
+
+	public void setModificationTime(String modificationTime) {
+		this.modificationTime = modificationTime;
+	}
+
+	public List<Molecule> getMolecules() {
+		return molecules;
+	}
+
+	public void setMolecules(ArrayList<Molecule> molecules) {
+		this.molecules = molecules;
+	}
+
+	@Override
+	public String toString() {
+		return "Project [id=" + id + ", description=" + description
+				+ ", creationTime=" + creationTime + ", modificationTime="
+				+ modificationTime + ", molecules=" + molecules.size() + "]";
+	}
+	
+	
 }
